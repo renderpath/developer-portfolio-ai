f5d9542d4a185e032b2c448e495502287b59e404
vera <pvk@cds.guru>
2026-05-19 12:12:23 +0300

    Support SMTP STARTTLS port

--- server/src/server.js
+++ server/src/server.js
@@ -18,7 +18,7 @@ app.use(express.json());
 const transporter = nodemailer.createTransport({
     host: process.env.SMTP_HOST,
     port: Number(process.env.SMTP_PORT),
-    secure: true,
+    secure: Number(process.env.SMTP_PORT) === 465,
     auth: {
         user: process.env.SMTP_EMAIL,
         pass: process.env.SMTP_PASSWORD,
